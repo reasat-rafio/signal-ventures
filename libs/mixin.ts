5b5d48e99d9ffb7217639a5ae984a25310232cfd
reasat-rafio <rafioar96@gmail.com>
2021-05-02 00:31:39 +0600

    window position  && types upgared

--- libs/mixin.ts
+++ libs/mixin.ts
@@ -1,7 +1,7 @@
 import { css } from 'styled-components'
 import { breakpoints } from './_variables'
 
-export const respondTo = Object.keys(breakpoints).reduce((accumulator, label) => {
+export const respondTo: any = Object.keys(breakpoints).reduce((accumulator, label) => {
     const _css: any = css
     accumulator[label] = (...args) => _css`
         @media (min-width: ${breakpoints[label]}) {
