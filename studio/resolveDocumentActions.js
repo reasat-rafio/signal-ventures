5f3980466e03acc2eff113e00df6e0c138a1beae
Christopher Fraser <chris@otterdev.io>
2021-05-10 15:59:50 +0800

    Resolver fix

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -1,12 +1,16 @@
-import defaultResolve, {
-    PublishAction,
-    DiscardChangesAction,
-} from 'part:@sanity/base/document-actions'
+import { PublishAction, DiscardChangesAction } from 'part:@sanity/base/document-actions'
+import allResolvers from 'all:part:@sanity/base/document-actions/resolver'
 
-export default function resolveDocumentActions(props) {
+function resolveDocumentActions(props) {
+    const otherResolvers = allResolvers.filter((r) => !r.isOwn)
+    const defaultResolve = otherResolvers[otherResolvers.length - 1]
+    console.log(defaultResolve(props))
     if (['site.nav'].includes(props.type)) {
         return [PublishAction, DiscardChangesAction]
     } else {
         return defaultResolve(props)
     }
 }
+
+resolveDocumentActions.isOwn = true
+export default resolveDocumentActions
