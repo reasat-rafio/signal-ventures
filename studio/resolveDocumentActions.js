568cc5e4bfea8b91c5223985490599cb85ec6973
reasat-rafio <rafioar96@gmail.com>
2021-05-10 14:01:25 +0600

    i18n issue

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -1,7 +1,19 @@
-import defaultResolve, {
-    PublishAction,
-    DiscardChangesAction,
-} from 'part:@sanity/base/document-actions'
+// import defaultResolve, {
+//     PublishAction,
+//     DiscardChangesAction,
+//     DeleteAction,
+// } from 'part:@sanity/base/document-actions'
+
+// export default function resolveDocumentActions(props) {
+//     if (['site.nav'].includes(props.type)) {
+//         return [PublishAction, DiscardChangesAction]
+//     } else {
+//         return defaultResolve(props)
+//     }
+// }
+
+import { PublishAction, DiscardChangesAction } from 'part:@sanity/base/document-actions'
+import defaultResolve from 'part:@sanity/base/document-actions/resolver'
 
 export default function resolveDocumentActions(props) {
     if (['site.nav'].includes(props.type)) {
