065c0eb388356fb5bfa5b6a5a8f25cfb02651337
reasat-rafio <rafioar96@gmail.com>
2021-05-10 12:23:06 +0600

    sanity resolve document action updated

--- studio/resolveDocumentActions.js
+++ studio/resolveDocumentActions.js
@@ -4,10 +4,9 @@ import defaultResolve, {
 } from 'part:@sanity/base/document-actions'
 
 export default function resolveDocumentActions(props) {
-    // if (['site.nav'].includes(props.type)) {
-    //     return [UnpublishAction, PublishAction]
-    // } else {
-    //     return defaultResolve(props)
-    // }
-    return [PublishAction, DiscardChangesAction]
+    if (['site.nav'].includes(props.type)) {
+        return [UnpublishAction, PublishAction]
+    } else {
+        return defaultResolve(props)
+    }
 }
