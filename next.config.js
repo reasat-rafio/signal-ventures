522bac221ad2b1cbf4305f7d2cdd6de2e03a2b52
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2023-01-09 16:53:28 -0500

    test on aws application error

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const config = {
         locales: ['en-SG', 'zh-SG'],
         defaultLocale: 'en-SG',
     },
-    // future: {
-    //     webpack5: true,
-    // },
+    reactStrictMode: true,
 }
-
-// const withFonts = require('next-fonts')
-// const withTM = require('next-transpile-modules')(['@react95/core'])
-
 module.exports = config
